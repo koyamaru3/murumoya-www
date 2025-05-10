172ebcf48ff0d14397193c13e16a5dd181ec9c8d
koyamaru3 <koyamaru@gmail.com>
2025-05-10 17:19:22 +0900

    20250510_goods

--- scripts/news/news.js
+++ scripts/news/news.js
@@ -1,9 +1,10 @@
 // 初期起動時の呼び出し
 function initNews() {
   const items = [
+    {date: "2025年5月第2週", content: "ミルモでポン！ クリアラバーコースター", link: "https://gashapon.jp/products/detail.php?jan_code=4582769837043000"},
     {date: "2025年4月下旬", content: "ミルモでポン！ ましゅもっちシリーズ", link: "https://www.fancy-fukuya.co.jp/topics/202504mirumodepon/"},
     {date: "2025年4月12日", content: "ミルモでポン！ アベイルとのコラボ商品発売", link: "https://x.com/gravail/status/1906292868911911399"},
-    {date: "2025年3月第4週", content: "ミルモでポン！もぐもぐパーティー発売", link: "https://gashapon.jp/products/detail.php?jan_code=4582769745140000"},
+//    {date: "2025年3月第4週", content: "ミルモでポン！もぐもぐパーティー発売", link: "https://gashapon.jp/products/detail.php?jan_code=4582769745140000"},
 //    {date: "2025年1月第4週", content: "ジェムリーズ ミルモでポン！発売", link: "https://gashapon.jp/products/detail.php?jan_code=4582769720284000"},
   ];
   let insertText = '';
